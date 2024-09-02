8016434c44c4f74e8ff95c7f523b07e8e749a458
Hardik Kalra <159179463+hardikkalra45@users.noreply.github.com>
2024-09-03 02:20:06 +0530

    Update obj_det.py

--- obj_det.py
+++ obj_det.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("yolov10x.pt")
 
-results = model("C:/Users/hardi/Downloads/person-bicycle-car-detection.mp4", save=True)
+results = model("C:/Users/hardi/Downloads/dog_bike_car.jpg", save=True)
 
 results[0].show()
